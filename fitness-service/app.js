df0323f172e11165697b8f998ebca0f03d08f7bb
Kshitij-Kumar123 <kshitijkumar.atom@gmail.com>
2024-02-20 11:33:22 -0500

    add major services restructure

--- fitness-service/app.js
+++ fitness-service/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 
-const PORT = process.env.PORT || 70;
+const PORT = process.env.PORT || 60;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
