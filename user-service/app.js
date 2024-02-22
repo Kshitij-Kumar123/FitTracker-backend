80fe69695e15127f9f88885f8d9726d3e9011ef7
Kshitij-Kumar123 <kshitijkumar.atom@gmail.com>
2024-02-21 20:27:27 -0500

    change user service port

--- user-service/app.js
+++ user-service/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 90;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
